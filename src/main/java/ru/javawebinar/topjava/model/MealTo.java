1dfd255ab807959b1599dd27e4cc6821da8fc196
TuMyP1985 <TuMyP1985@mail.ru>
2022-02-04 16:58:34 +0300

    HW01

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class MealTo {
     private final LocalDateTime dateTime;
@@ -10,6 +11,26 @@ public class MealTo {
     private final int calories;
 
     private final boolean excess;
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExcess() {
+        return excess;
+    }
+
+    public String getDateFormatted() {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").format(dateTime);
+    }
 
     public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.dateTime = dateTime;
